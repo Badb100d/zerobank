5be7cd0d0b73c8941f39c71ba94b9c9982f62285
badb100d <260448993@qq.com>
2020-01-16 14:20:22 +0800

    change init_set link to file

--- init_set.sh
+++ init_set.sh
@@ -1,1 +1,12 @@
-../init_set.sh
+#/bin/bash
+
+echo "iwconfig wlan0 power off" >> /etc/rc.local
+#echo "service ssh start" >> /etc/rc.local
+update-rc.d ssh start
+apt-get install -y vim tmux
+
+#nohup /root/power_control/reboot_switch.sh > /dev/null &
+
+#nohup /root/modem_start.sh > /tmp/modem.log 2>&1 &
+
+#nohup /root/restart_notify.sh > /dev/null 2>&1 &
